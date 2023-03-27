b902972a9f77b7571a0b79d8ed9939451380482e
Jaxaar <jax1664-cs@yahoo.com>
2023-03-26 23:22:20 -0400

    It works
    No selector, but randomizer works

--- src/main.js
+++ src/main.js
@@ -3,13 +3,29 @@
 
 //Initialization
 var main = {};
+var options = ["a", "b", "c", "d", "e", "r", "d2?"]
 
 
 main.generatePairings = function() {
+    options = ["a", "b", "c", "d", "e", "r", "d2"]
     var str = "<br>"
-    for(var i = 0; i < 10; i++){
-        str = str + i + " num" + "<br>"
+    while(options.length > 3){
+        x = Math.floor(Math.random() * options.length)
+        str += options.splice(x,1)[0] + ", "
+
+        y = Math.floor(Math.random() * options.length)
+        str += options.splice(y,1)[0]
+        str += "<br>"
     }
+
+    while(options.length > 0){
+        str += options.splice(0,1)[0]
+        if(options.length > 0){
+            str += ", "
+        }
+    }
+
+
     document.getElementById('pairData').innerHTML = "Pairings: " + str
 
 }
