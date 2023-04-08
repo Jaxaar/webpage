b2519903fe7cc40bcc98b770b73ed04d3dda7f38
@EthanStyle <brownev19@gcc.edu>
2023-04-08 16:02:47 -0400

    Programmatically make paired fencers.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const checkboxes = document.getElementsByTagName("input");
-    for(const element of checkboxes) {
-        element.setAttribute('autocomplete', "off");
-        element.classList.add('btn-check');
-    }
-    const checkboxLabels = document.getElementsByTagName("label");
-    for(const element of checkboxLabels) {
-        element.classList.add('btn', 'btn-outline-primary');
-    }
-    const pairLDemo = document.getElementById('pair-l');
-    const pairRDemo = document.getElementById('pair-r');
-    pairLDemo.addEventListener('mouseover', onFencerHovered);
-    pairLDemo.addEventListener('mouseleave', onFencerUnhovered);
-    pairRDemo.addEventListener('mouseover', onFencerHovered);
-    pairRDemo.addEventListener('mouseleave', onFencerUnhovered);
+	const checkboxes = document.getElementsByTagName("input");
+	for (const element of checkboxes) {
+		element.setAttribute('autocomplete', "off");
+		element.classList.add('btn-check');
+	}
+	const checkboxLabels = document.getElementsByTagName("label");
+	for (const element of checkboxLabels) {
+		element.classList.add('btn', 'btn-outline-primary');
+	}
 });
 
 
@@ -22,74 +16,101 @@ var main = {};
 // var o = ["a", "b", "c", "d", "e", "r"]
 var people = []
 
-main.getPeople = function() {
-    return JSON.parse(JSON.stringify(people))
-    // return structuredClone(array1);
+main.getPeople = function () {
+	return JSON.parse(JSON.stringify(people))
+	// return structuredClone(array1);
 }
 
-main.togglePerson = function(str) {
-    if(people.includes(str)){
-        people.splice(people.indexOf(str),1)
-    }
-    else{
-        people.push(str)
-    }
-    console.log(people)
+main.togglePerson = function (str) {
+	if (people.includes(str)) {
+		people.splice(people.indexOf(str), 1)
+	}
+	else {
+		people.push(str)
+	}
+	console.log(people)
 }
 
 {/* Make this structure for each pair:
 <div class="pair-group">
-    <div class="pair left">E</div>
-    <div class="pair right">C</div>
-</div> 
+    <div class="pair left">Fencer 1</div>
+    <div class="pair right">Fencer 2</div>
+</div>
 */}
-main.generatePairings = function() {
-    options = main.getPeople()
-    var str = "<br>"
-    while(options.length > 3){
-        x = Math.floor(Math.random() * options.length)
-        str += options.splice(x,1)[0] + ", "
+main.generatePairings = function () {
+	options = main.getPeople();
+	shuffleArray(options);
+	const pairsDiv = document.getElementById('pairs');
+	const crowdDiv = document.getElementById('crowd');
 
-        y = Math.floor(Math.random() * options.length)
-        str += options.splice(y,1)[0]
-        str += "<br>"
-    }
+	const groupedFencers = groupFencers(options, 2);
+	const fencersHTML = [];
+	var crowd = [];
 
-    while(options.length > 0){
-        str += options.splice(0,1)[0]
-        if(options.length > 0){
-            str += ", "
-        }
-    }
+	for (const fencers of groupedFencers) {
+		if (fencers.length == 2) {
+			fencersHTML.push(makePairHTML(fencers[0], fencers[1]));
+		} else {
+			crowd = remainderHTML(fencers);
+		}
+	}
+	pairsDiv.replaceChildren(...fencersHTML);
+	crowdDiv.replaceChildren(...crowd);
+}
 
+// from user ashleedawg at https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
 
-    document.getElementById('pairData').innerHTML = "Pairings: " + str
-
+function groupFencers(options, chunkSize) {
+	const groups = [];
+	for (let i = 0; i < options.length; i += chunkSize) {
+		const chunk = options.slice(i, i + chunkSize);
+		groups.push(chunk);
+	}
+	return groups;
 }
 
 function onFencerHovered(event) {
-    const node = event.target;
-    const opponentNode = !!node.nextElementSibling ? node.nextElementSibling : node.previousElementSibling;
-    opponentNode.classList.add('hide');
+	const node = event.target;
+	const opponentNode = !!node.nextElementSibling ? node.nextElementSibling : node.previousElementSibling;
+	opponentNode.classList.add('hide');
 }
 
 function onFencerUnhovered(event) {
-    const node = event.target;
-    const opponentNode = !!node.nextElementSibling ? node.nextElementSibling : node.previousElementSibling;
-    opponentNode.classList.remove('hide');
+	const node = event.target;
+	const opponentNode = !!node.nextElementSibling ? node.nextElementSibling : node.previousElementSibling;
+	opponentNode.classList.remove('hide');
 }
 
-function makePairHTML() {
-    const pairGroup = document.createElement("div");
-    pairGroup.classList.add('pair-group');
-    const left = document.createElement("div");
-    left.classList.add('pair left');
-    left.addEventListener('mouseover', onFencerHovered);
-    left.addEventListener('mouseleave', onFencerUnhovered);
-    const right = document.createElement("div");
-    right.classList.add('pair right');
-    right.addEventListener('mouseover', onFencerHovered);
-    right.addEventListener('mouseleave', onFencerUnhovered);
-    pairGroup.append(left, right);
-    return pairGroup;
+function makePairHTML(leftFencer, rightFencer) {
+	const pairGroup = document.createElement("div");
+	pairGroup.classList.add('pair-group');
+	const left = document.createElement("div");
+	left.classList.add('pair', 'left');
+	left.addEventListener('mouseover', onFencerHovered);
+	left.addEventListener('mouseleave', onFencerUnhovered);
+	left.innerText = leftFencer;
+	const right = document.createElement("div");
+	right.classList.add('pair', 'right');
+	right.addEventListener('mouseover', onFencerHovered);
+	right.addEventListener('mouseleave', onFencerUnhovered);
+	right.innerText = rightFencer;
+	pairGroup.append(left, right);
+	return pairGroup;
+}
+
+function remainderHTML(remainder) {
+	const crowdElems = [];
+	for (const fencer of remainder) {
+		const spectator = document.createElement("div");
+		spectator.classList.add('spectator');
+		spectator.innerText = remainder;
+		crowdElems.push(spectator);
+	}
+	return crowdElems;
 }
