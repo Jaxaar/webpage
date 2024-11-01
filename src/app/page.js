b34f601266d1652823dd873e7a135e1770819259
Jaxaar <73668604+Jaxaar@users.noreply.github.com>
2024-11-01 18:31:43 -0400

    Very basic Next.js & React setup

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h className = "font-bold">
+        Hello!
+      </h>
+      <div>
+        This is Not a scam, Trust
+      </div>
+    </div>
+  );
+}
