83c81512718031be246e2948d1ab151823b25e44
Jaxaar <73668604+Jaxaar@users.noreply.github.com>
2025-03-22 17:54:39 -0400

    Test api 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
+'use client'
 import Image from "next/image";
 
 export default function Home() {
+    async function testFetch(){
+        const result = await fetch(`/webpage/api/test`)
+        console.log(result)
+    }
+
     return (
         <div>
             <div className="font-bold">
@@ -8,6 +14,20 @@ export default function Home() {
             </div>
             <div>
                 WIP... I Hope... Maybe got shelved... Idk
+            </div>
+            <div>
+                <div onClick={testFetch}>
+                        Test
+                </div>
+            </div>
+            <div>
+            {/* <iframe width="560" height="315" 
+                src="https://www.youtube.com/embed/fOk8Tm815lE?si=Z_uSqLuBfIaVq1a9&amp;start=846&autoplay=true&end=850" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen></iframe> */}
             </div>
         </div>
     );
